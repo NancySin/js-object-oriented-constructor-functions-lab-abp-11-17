50a3d95f9959e1a7ac62f5be73fee0819ae926f3
nancy sin <nsin@sbcglobal.net>
2017-12-17 20:39:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function Scooter ( year, color, model){
  }
 
 function Driver ( name, age, experiene){
-  this.name = name
-  this.age = age
-  this.experience = experience
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
 }
