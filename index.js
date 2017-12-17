8c2c4c169f9c3b10b2a8522def7890adefb9e06b
nancy sin <nsin@sbcglobal.net>
2017-12-17 20:35:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
-Function Scooter( year, color, model){
- this.year = year
- this.color = color
- this.model = model
+Function Scooter ( year, color, model){
+ this.year = year;
+ this.color = color;
+ this.model = model;
  }
- 
